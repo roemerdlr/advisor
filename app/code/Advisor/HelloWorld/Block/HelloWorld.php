853c5c1d543d978fe28a7cd141c755eb6dc9d5bf
Romer de los Rios <romer@wagento.com>
2016-01-06 18:05:36 -0400

    fixed module to display in frontend

---
+++ app/code/Advisor/HelloWorld/Block/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Advisor\HelloWorld\Block;
+
+class HelloWorld extends \Magento\Framework\View\Element\Template {
+
+    public function _prepareLayout() {
+        return parent::_prepareLayout();
+    }
+
+}
